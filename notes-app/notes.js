e6f0dd8825d351c0dd5aff8daa0c9230dbaa5d83
xDamneDx <xdamnedx@gmail.com>
2022-11-02 11:23:52 +0200

    fix add note duplication search

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 
 const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter((note) => note.title === title);
+  const duplicateNote = notes.find((note) => note.title === title);
 
-  if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({
       title,
       body,
