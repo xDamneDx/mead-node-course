7cc370ce35e8a945908547149169115211efc4b0
xDamneDx <xdamnedx@gmail.com>
2022-11-07 19:12:06 +0200

    setup partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 // Define path for express config:
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlebars engine & views location:
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve:
 app.use(express.static(publicDirectoryPath));
@@ -17,18 +20,21 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather App",
+    name: "xDamneDx",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About",
+    name: "xDamneDx",
   });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
     title: "Help",
+    name: "xDamneDx",
   });
 });
 
